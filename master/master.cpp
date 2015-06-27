30524bcc23463d39853de2c4348b2d4f97404e09
Brian Chen <23047@student.bbc.qld.edu.au>
2015-06-27 17:31:14 +1000

    divided main loop into functions

--- master/master.cpp
+++ master/master.cpp
@@ -12,13 +12,16 @@
 #include <SPI.h>
 #include <slaves.h>
 #include <piCommon.h>
-#include <PixyI2C.h>
+#include <PixyI2C.h> // modified for i2c_t3
 #include <SRF08.h>
 #include <fastTrig.h>
 #include <PID.h>
 #include <DebugUtils.h>
 
 #define LED 13
+
+#define RAMMING_SPEED 200
+#define NORMAL_SPEED 130
 
 uint32_t loopCount = 0;
 
@@ -61,10 +64,13 @@ int16_t backspinSpeed = 0;
 /*                       Kicker                           */
 /**********************************************************/
 #define KICK_TIME 4000
-#define KICK_DURATION 80
+#define KICK_DURATION 90
 
 #define KICK_SIG 21
 #define KICK_ANA A3
+
+bool kicking = false;
+elapsedMillis capChargedTime = 0;
 
 /**********************************************************/
 /*					     Camera   						  */
@@ -119,6 +125,33 @@ uint8_t tsopData[24] = {0};
 
 bool ballInZone = false;
 
+void kick(){
+	if (!kicking){
+		// not yet kicking start kick
+		kicking = true;
+		digitalWriteFast(KICK_SIG, HIGH);
+		capChargedTime = 0; // now effectively discharge time
+		// Serial.print(analogRead(KICK_ANA));
+		// Serial.println("KICK");
+	}
+}
+
+void checkEndKick(){
+	if (kicking){
+		if (capChargedTime >= KICK_DURATION){
+			// end kick
+			kicking = false;
+			digitalWriteFast(KICK_SIG, LOW);
+			capChargedTime = 0;
+			// Serial.print(analogRead(KICK_ANA));
+			// Serial.println("END");
+		}
+	}
+	else{
+		digitalWriteFast(KICK_SIG, LOW);
+	}
+}
+
 void calibIMUOffset(){
 	for (int i = 0; i < 50; i++){
 		Slave1.requestPacket(SLAVE1_COMMANDS::REQUEST_STANDARD_PACKET);
@@ -134,7 +167,148 @@ void calibIMUOffset(){
 	bearing_offset /= 50;
 }
 
-int main(void){
+void getSlave1Data(){
+	Slave1.requestPacket(SLAVE1_COMMANDS::REQUEST_STANDARD_PACKET);
+	Slave1.receivePacket(inBuffer, 7, true);
+	
+	f2b.b[0] = inBuffer[2];
+	f2b.b[1] = inBuffer[3];
+	f2b.b[2] = inBuffer[4];
+	f2b.b[3] = inBuffer[5];
+
+	bearing = -f2b.f - bearing_offset;
+	bearing_int = (int32_t)(bearing);
+	TOBEARING180(bearing_int);
+	TOBEARING180(bearing);
+}
+
+void getSLave2Data(){
+	tsopAngleByte = Slave2.getTSOP_ANGLE_BYTE();
+	delayMicroseconds(50); // do i need this here?
+	tsopStrength = Slave2.getTSOP_STRENGTH();
+
+	ballDistance = 180 - tsopStrength;
+
+	tsopAngle = tsopAngleByte * 360/255;
+	TOBEARING180(tsopAngle);
+	tsopAngle_r_goal = tsopAngle - goalAngle;
+	TOBEARING180(tsopAngle_r_goal);
+}
+
+void getGoalData(){
+	goalArea = pixy1.blocks[0].width * pixy1.blocks[0].height;
+
+	if (blocks > 0
+		&& goalArea > MIN_BLOCK_AREA
+		&& abs(bearing) < 90
+		){
+		goalDetected = true;
+		goalAngle = (pixy1.blocks[0].x - 160) * 75 / 360;	
+		goalAngle_rel_field = goalAngle + bearing_int;
+		lGoalDetectTime = 0;		
+	}
+	else if (lGoalDetectTime > 100){
+		// hasn't seen goal for 100ms
+		goalDetected = false;
+		goalAngle = 0;
+	}
+}
+
+void getBackspinSpeed()}{
+	if (tsopStrength > 70){
+		if (abs(targetDir) < 60){
+			if (ballInZone){
+				// we're going forwards and we have the ball
+				// no need for too much spin
+				backspinSpeed = 0;
+			}
+			else{
+				// forwards but ball not here yet!
+				backspinSpeed = 200;
+			}
+		}
+		else{
+			if (ballInZone){
+				// we're not going forwards but we have the ball
+				// best to spin a bit more as we need to guide the ball more
+				backspinSpeed = 150;
+			}
+			else{
+				// most common. Should only spin when ball is in front
+				if (abs(tsopAngle) < 60){
+					backspinSpeed = 120;
+				}
+				else{
+					// no chance of getting ball any time soon
+					backspinSpeed = 0;
+				}
+			}
+		}
+	}
+	else{
+		// no ball detected!
+		backspinSpeed = 0;
+	}
+}
+
+void checkBallInZone(){
+	if (analogRead(LASER_SIG) < LASER_REF 
+	 && abs(tsopAngle) < 30
+	 && tsopStrength > 150){
+		ballInZone = true;
+	}
+	else{
+		ballInZone = false;
+	}
+}
+
+void serialDebug(){
+	Serial.printf("%d\t%d\t%d\t%.1f\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
+				  micros(),
+				  goalAngle_rel_field,
+				  goalArea,
+				  bearing,
+				  backDistance,
+				  rightDistance,
+				  leftDistance,
+				  tsopAngle,
+				  ballDistance,
+				  ballInZone,
+				  targetDir,
+				  targetVelocity,
+				  rotatationCorrection);
+
+	if(Serial.available()){
+		char serialCommand = Serial.read();
+		CLEARSERIAL(); // make sure you only read first byte and clear everything else
+		if (serialCommand == 'i'){
+			Serial.println("DEBUG INFO");
+			Serial.printf("%s %s %s %s %s %s %s %s %s %s %s %s %s %s \n",
+				  "micros()",
+				  "goalAngle_rel_field",
+				  "goalArea",
+				  "bearing",
+				  "backDistance",
+				  "rightDistance",
+				  "leftDistance",
+				  "tsopAngle",
+				  "ballDistance",
+				  "ballInZone",
+				  "targetDir",
+				  "targetVelocity",
+				  "rotatationCorrection");
+			Serial.println("PRESS ANY KEY TO CONTINUE");
+			// wait for key
+			while(!Serial.available());
+			CLEARSERIAL();
+		}
+		else{
+			Serial.println("UNKNOWN COMMAND");
+		}
+	}
+}
+
+int main(void){	
 	Serial.begin(115200);
 
 	Wire.begin(I2C_MASTER, 0x00, I2C_PINS_18_19, I2C_PULLUP_EXT, I2C_RATE_400);
@@ -152,6 +326,8 @@ int main(void){
 	pinMode(KICK_SIG, OUTPUT);
 	pinMode(KICK_ANA, INPUT);
 
+	digitalWrite(KICK_SIG, LOW);
+
 	digitalWrite(LED, HIGH);
 
 	delay(1000);
@@ -168,78 +344,35 @@ int main(void){
 		}
 
 		/* orientation/imu */
-		Slave1.requestPacket(SLAVE1_COMMANDS::REQUEST_STANDARD_PACKET);
-		Slave1.receivePacket(inBuffer, 7, true);
-		
-		f2b.b[0] = inBuffer[2];
-		f2b.b[1] = inBuffer[3];
-		f2b.b[2] = inBuffer[4];
-		f2b.b[3] = inBuffer[5];
-
-		bearing = -f2b.f - bearing_offset;
-		bearing_int = (int32_t)(bearing);
-		TOBEARING180(bearing_int);
-		TOBEARING180(bearing);
-
+		getSlave1Data();
 		/* end orientation/imu */
 
 		/* tsops */
-		tsopAngleByte = Slave2.getTSOP_ANGLE_BYTE();
-		delayMicroseconds(50); // do i need this here?
-		tsopStrength = Slave2.getTSOP_STRENGTH();
-		//Slave2.getTSOPAngleStrength(tsopAngleByte, tsopStrength);
-
-		ballDistance = 180 - tsopStrength;
-
-		tsopAngle = tsopAngleByte * 360/255;
-		TOBEARING180(tsopAngle);
-		tsopAngle_r_goal = tsopAngle - goalAngle;
-		TOBEARING180(tsopAngle_r_goal);
-
+		getSLave2Data();
 		/* end tsops */
 
 		/* goal detection */
-
-		goalArea = pixy1.blocks[0].width * pixy1.blocks[0].height;
-
-		if (blocks > 0
-			&& goalArea > MIN_BLOCK_AREA
-			&& abs(bearing) < 90
-			){
-			goalDetected = true;
-			goalAngle = (pixy1.blocks[0].x - 160) * 75 / 360;	
-			goalAngle_rel_field = goalAngle + bearing_int;
-			lGoalDetectTime = 0;		
-		}
-		else if (lGoalDetectTime > 100){
-			// hasn't seen goal for 100ms
-			goalDetected = false;
-			goalAngle = 0;
-		}
+		getGoalData();
 		/* end goal detection */
 
 		/* face forwards */
 		if (goalDetected){
 			targetBearing = goalAngle_rel_field;
 		}
+		else{
+			targetBearing = 0;
+		}
 
 		bearingPID.update();
 		/* end face forwards */
 
 		/* ball in zone */
-		if (analogRead(LASER_SIG) < LASER_REF 
-		 && abs(tsopAngle) < 30
-		 && tsopStrength > 150){
-			ballInZone = true;
-		}
-		else{
-			ballInZone = false;
-		}
+		checkBallInZone();
 		/* end ball in zone */
 
 		/*movement control*/
 
-		targetVelocity = 130;
+		targetVelocity = NORMAL_SPEED;
 
 		orbit_k = 1;
 
@@ -279,103 +412,31 @@ int main(void){
 		// }
 
 
-		if (ballInZone && abs(goalAngle) < 10){
+		if (ballInZone && goalDetected && abs(goalAngle) < 10){
 			// GO!
 			targetDir = goalAngle;
-			targetVelocity = 200;
+			targetVelocity = RAMMING_SPEED;
+			// kick!
+			if (capChargedTime > KICK_TIME){
+				kick();
+			}
 		}
-		
-
-		if (targetDir < 0) targetDir += 360;
-		targetDir = targetDir * 255/360;
+		checkEndKick();
 
 		/* end movement control */
 
 		/* backspin control */
-		if (tsopStrength > 70){
-			if (abs(targetDir) < 60){
-				if (ballInZone){
-					// we're going forwards and we have the ball
-					// no need for too much spin
-					backspinSpeed = 0;
-				}
-				else{
-					// forwards but ball not here yet!
-					backspinSpeed = 120;
-				}
-			}
-			else{
-				if (ballInZone){
-					// we're not going forwards but we have the ball
-					// best to spin a bit more as we need to guide the ball more
-					backspinSpeed = 150;
-				}
-				else{
-					// most common. Should only spin when ball is in front
-					if (abs(tsopAngle) < 60){
-						backspinSpeed = 255;
-					}
-					else{
-						// no chance of getting ball any time soon
-						backspinSpeed = 0;
-					}
-				}
-			}
-		}
-		else{
-			// no ball detected!
-			backspinSpeed = 0;
-		}
-		
+		getBackspinSpeed();
 		/* end backspin control */
 
-		Slave3.moveRobot((uint8_t)targetDir, targetVelocity, rotatationCorrection);
+		if (targetDir < 0) targetDir += 360;
+		targetDir = targetDir * 255/360;
+
+		// Slave3.moveRobot((uint8_t)targetDir, targetVelocity, rotatationCorrection);
 		Slave3.moveMotorE(backspinSpeed);
 
 		/* debugging */
-		Serial.printf("%d\t%d\t%d\t%.1f\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\n",
-					  micros(),
-					  goalAngle_rel_field,
-					  goalArea,
-					  bearing,
-					  backDistance,
-					  rightDistance,
-					  leftDistance,
-					  tsopAngle,
-					  ballDistance,
-					  ballInZone,
-					  targetDir,
-					  targetVelocity,
-					  rotatationCorrection);
-
-		if(Serial.available()){
-			char serialCommand = Serial.read();
-			CLEARSERIAL(); // make sure you only read first byte and clear everything else
-			if (serialCommand == 'i'){
-				Serial.println("DEBUG INFO");
-				Serial.printf("%s %s %s %s %s %s %s %s %s %s %s %s %s %s \n",
-					  "micros()",
-					  "goalAngle_rel_field",
-					  "goalArea",
-					  "bearing",
-					  "backDistance",
-					  "rightDistance",
-					  "leftDistance",
-					  "tsopAngle",
-					  "ballDistance",
-					  "ballInZone",
-					  "targetDir",
-					  "targetVelocity",
-					  "rotatationCorrection");
-				Serial.println("PRESS ANY KEY TO CONTINUE");
-				// wait for key
-				while(!Serial.available());
-				CLEARSERIAL();
-			}
-			else{
-				Serial.println("UNKNOWN COMMAND");
-			}
-		}
+		serialDebug();
 		/* end debugging */
 
 		loopCount++;  // update loop count
